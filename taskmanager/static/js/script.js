82a7d16188844c88d05e3763edcccb9d23712177
Mr-KulwinderSingh Sidhu <k29singh@gmail.com>
2023-09-20 19:22:00 +0000

    Add the basic structure in base.html with materialize classes and structure for the main page, and add script.js and style.css together with their links and cdns to join the whole set and work together

---
+++ taskmanager/static/js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // sidenave initializeation
+    let sidenav = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(sidenav);
+  });
